9dfe87beed42334d942204c0fd531af9971dc4fd
Brian Prickril <bprickril22@gmail.com>
2020-04-01 01:35:07 -0400

    created cards for react projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,44 +1,141 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell } from 'react-mdl'
+import React, { Component } from 'react'
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from 'react-mdl'
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { activeTab: 0 };
-    }
+  constructor (props) {
+    super(props)
+    this.state = { activeTab: 0 }
+  }
 
-    toggleCategoris() {
-        if(this.state.activeTab === 0) {
-            return (
-                <div><h1>This is React</h1></div>
-            )
-        } else if (this.state.activeTab === 1) {
-            return (
-                <div><h1>This is MongoDB</h1></div>
-            )
-        } else if (this.state.activeTab === 2) {
-            return (
-                <div><h1>This is Express</h1></div>
-            )
-        }
-    }
+  toggleCategories () {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className='projects-grid'>
 
-    render() {
-        return(
-            <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>React</Tab>
-                    <Tab>MongoDB</Tab>
-                    <Tab>Express</Tab>
-                </Tabs>
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: 'black',
+                height: '176px',
+                background:
+                  'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover'
+              }}
+            >
+              React Project #1
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+              Ipsum Lorem Ipsum Lorem Ipsum
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Deployed App</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+            </Card>
 
-                <section className="projects-grid">
-                    {this.toggleCategoris()}
-                </section>
-            
-            </div>
-        )
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: 'black',
+                height: '176px',
+                background:
+                  'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover'
+              }}
+            >
+              React Project #2
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+              Ipsum Lorem Ipsum Lorem Ipsum
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Deployed App</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+            </Card>
+
+            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle
+              style={{
+                color: 'black',
+                height: '176px',
+                background:
+                  'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png) center / cover'
+              }}
+            >
+              React Project #3
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+              Ipsum Lorem Ipsum Lorem Ipsum
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Deployed App</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name='share' />
+            </CardMenu>
+            </Card>
+
+        </div>
+      )
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      )
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is Express</h1>
+        </div>
+      )
     }
+  }
+
+  render () {
+    return (
+      <div className='category-tabs'>
+
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>Express</Tab>
+        </Tabs>
+
+          <Grid>
+            <Cell col={12}>
+              <div className='content'>{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+
+      </div>
+    )
+  }
 }
 
 export default Projects
