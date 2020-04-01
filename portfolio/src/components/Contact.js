d7aa9f775afcb1c4a97ea36fcbad05db7e3387ea
Brian Prickril <bprickril22@gmail.com>
2020-04-01 12:54:06 -0400

    created avatar and name on contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl'
 
 class Contact extends Component {
     render() {
         return(
-            <div>
-                <h1>Contact Page</h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Brian Prickril</h2>
+                        <img 
+                        src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                        alt="avatar"
+                        style={{height: "250px"}}/>
+                    </Cell>
+                    <Cell col={6}>halfpage</Cell>
+                </Grid>
+                
             </div>
         )
     }
