b3414aa16ec7545e4f5406a816d3643161c13667
Brian Prickril <bprickril22@gmail.com>
2020-04-01 13:25:53 -0400

    finished contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,24 +1,65 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl'
+import React, { Component } from 'react'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
 
 class Contact extends Component {
-    render() {
-        return(
-            <div className="contact-body">
-                <Grid className="contact-grid">
-                    <Cell col={6}>
-                        <h2>Brian Prickril</h2>
-                        <img 
-                        src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-                        alt="avatar"
-                        style={{height: "250px"}}/>
-                    </Cell>
-                    <Cell col={6}>halfpage</Cell>
-                </Grid>
-                
+  render () {
+    return (
+      <div className='contact-body'>
+        <Grid className='contact-grid'>
+          <Cell col={6}>
+            <h2>Brian Prickril</h2>
+            <img
+              src='https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png'
+              alt='avatar'
+              style={{ height: '300px' }}
+            />
+
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was.
+            </p>
+          </Cell>
+
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+
+            {/* Cell Phone Number */}
+            <List>
+              <ListItem>
+                <ListItemContent style={{fontSize: "30px", fontFamily: "Anton"}}>
+                  <i className="fa fa-phone-square" aria-hidden="true" />  
+                    (123) 456-7890
+                    
+                    </ListItemContent>
+              </ListItem>
+
+              {/* Email Adress */}
+              <ListItem>
+                <ListItemContent style={{fontSize: "30px", fontFamily: "Anton"}}>
+                  <i className="fa fa-envelope" aria-hidden="true" />  
+                    bprickril22@gmail.com
+                    
+                    </ListItemContent>
+              </ListItem>
+
+            </List>
+
+
             </div>
-        )
-    }
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default Contact
