df0e3f3e5a468de73d91dc351420d46bc6d23b2c
Brian Prickril <bprickril22@gmail.com>
2020-04-01 00:10:34 -0400

    added banner and social links to landing page

--- portfolio/src/components/LandingPage.js
+++ portfolio/src/components/LandingPage.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Grid, Cell } from 'react-mdl'
 
 class LandingPage extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Landing Page</h1>
+  render () {
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img
+              src='https://www.shareicon.net/data/512x512/2015/09/18/103160_man_512x512.png'
+              alt='avatar'
+              className='avatar-img'
+            />
+
+            <div className='banner-text'>
+              <h1>Full-Stack Web Developer</h1>
+              <hr />
+              <p>HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB</p>
+
+              <div className='social-links'>
+                {/*/ LinkedIn */}
+                <a
+                  href='http://www.google.com'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <i className='fa fa-linkedin-square' aria-hidden='true' />
+                </a>
+
+                {/*/ Github */}
+                <a
+                  href='http://www.google.com'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <i className='fa fa-github-square' aria-hidden='true' />
+                </a>
+              </div>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default LandingPage
