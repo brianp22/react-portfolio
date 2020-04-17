5f4afb349f96751809b3e03cf045223ccbe5a86a
Brian Prickril <bprickril22@gmail.com>
2020-04-17 16:33:20 -0400

    added aws cert button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "react-mdl";
 import Resume from '../documents/resume.pdf'
+import AWS from '../documents/aws_cert.pdf'
 
 
 function Footer() {
@@ -11,6 +12,8 @@ function Footer() {
             <Button style={{color: "white"}} href="https://github.com/brianp22" target="_blank">GitHub
             </Button>
             <Button style={{color: "white"}} href="https://www.linkedin.com/in/brianprickril/" target="_blank">LinkedIn
+            </Button>
+            <Button style={{color: "white"}} href={AWS}target="_blank">AWS CERT
             </Button>
             <br/>
             © 2020
