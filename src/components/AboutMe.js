a0180319a36b456092b4dccb5cf9f72955cfc98c
Brian Prickril <bprickril22@gmail.com>
2020-04-17 14:08:59 -0400

    updated the about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import headShot from "../images/mypicture.jpeg";
 
 class AboutMe extends Component {
-    render() {
-        return(
-            <div>
-                <h1>About Me Page</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={12}>
+            <h2>Brian Prickril</h2>
+            <img src={headShot} alt="avatar" style={{ height: "300px" }} />
+
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Brian is currently a full-stack development student who will be
+              receiving his certification from The University of Pennyslvania in
+              May 2020. During this coding bootcamp, Brian will have gained
+              working knowledge of JavaScript and other modern web technologies--including, but not limited to: HTML/CSS, Node, React, Express,
+              MongoDB, and MySQL.{" "}
+            </p>
+
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Brian is also particularly interested in opportunities in the
+              fintech industry. His passion for web development stemmed from the
+              discovery of Bitcoin and decentralized finance protocols. He
+              believes that we are entering a new paradigm in payment solutions
+              and is interested in exploring those technologies upon completion
+              of his bootcamp. In his free time, Brian enjoys exploring the
+              outdoors, playing video games, all things music-related, and
+              eating new foods with friends.
+            </p>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default AboutMe
+export default AboutMe;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
-import headShot from "../images/mypicture.jpeg"
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import headShot from "../images/mypicture.jpeg";
 
 class Contact extends Component {
-  render () {
+  render() {
     return (
-      <div className='contact-body'>
-        <Grid className='contact-grid'>
+      <div className="contact-body">
+        <Grid className="contact-grid">
           <Cell col={6}>
             <h2>Brian Prickril</h2>
-            <img
-              src={headShot}
-              alt='avatar'
-              style={{ height: '300px' }}
-            />
+            <img src={headShot} alt="avatar" style={{ height: "300px" }} />
 
-            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
-              I am currently pursuing entry-level opportunities in the tech industry.  Please do not hesitate to contact me if you think I am a fit for your team.
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              I am currently pursuing entry-level opportunities in the tech
+              industry. Please do not hesitate to contact me if you think I am a
+              fit for your team.
             </p>
           </Cell>
 
@@ -25,37 +23,33 @@ class Contact extends Component {
             <hr />
 
             <div className="contact-list">
-
-            {/* Cell Phone Number */}
-            <List>
-              <ListItem>
-                <ListItemContent style={{fontSize: "30px", fontFamily: "Anton"}}>
-                  <i className="fa fa-phone-square" aria-hidden="true" />  
+              {/* Cell Phone Number */}
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
                     (609) 923-2399
-                    
-                    </ListItemContent>
-              </ListItem>
+                  </ListItemContent>
+                </ListItem>
 
-              {/* Email Adress */}
-              <ListItem>
-                <ListItemContent style={{fontSize: "30px", fontFamily: "Anton"}}>
-                  <i className="fa fa-envelope" aria-hidden="true" />  
+                {/* Email Adress */}
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
                     bprickril22@gmail.com
-                    
-                    </ListItemContent>
-              </ListItem>
-
-            </List>
-
-
+                  </ListItemContent>
+                </ListItem>
+              </List>
             </div>
-
-
           </Cell>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
-export default Contact
+export default Contact;
